3ed7bf9b8661758fe7c52d35af5a7d0834d52945
Aniket Das <aniket.das.in@gmail.com>
2023-11-12 08:02:08 +0530

    initial commit

---
+++ SQL/mysql.initial.sql
@@ -0,0 +1,10 @@
+-- MySQL table for nonce_login
+
+CREATE TABLE login_nonces (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nonce VARCHAR(255) NOT NULL,
+    expires DATETIME NOT NULL,
+    user VARCHAR(255) NOT NULL,
+    pass VARCHAR(255) NOT NULL,
+    host VARCHAR(255) NOT NULL
+);
